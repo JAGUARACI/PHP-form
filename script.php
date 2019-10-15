6a253c980f363e48f3552327c22c0b7b4c469bf7
JAGUARACI <jaguaracisudu@gmaile.com>
2019-10-15 16:12:12 -0300

    continuar no index.php

--- script.php
+++ script.php
@@ -20,37 +20,37 @@ $idade = $_POST['idade'];
 
 if(empty($nome))
 {$_SESSION['mensagem-de-erro'] = 'O nome não pode ser vazio por favor preencha-o novamente';
-header(string'location: index.php');
+    header(string'location:index.php');
 }
 
 if(strlen($nome) < 3)
-{echo 'O nome deve conter mais de 3 caracteres';
-return;
+{$_SESSION['mensagem-de-erro'] = 'O nome não pode ter menos de três caracteres';
+    header( string: 'location: index.php');
 }
 
 if(strlen($nome) > 40)
-{echo 'O nome é muito extenso';
-return;
-}
+{$_SESSION['mensagem-de-erro'] = 'O nome não pode ser ter mais de quarenta caracteres';
+    header( string: 'location: index.php');
+    }
 
 if(!is_numeric($idade))
-{echo 'informe um número para idade';
-}
-return;
+{$_SESSION['mensagem-de-erro'] = 'Informe um número para idade';
+    header( string: 'location: index.php');
+    }
 
 if($idade >= 6 && $idade <=12)
 
 {for($i = 0; $i <= count ($categorias); $i++)
 
 {if($categorias[$i] == 'infantil')
-echo "O nadador",$nome, "compete na categoria",$categorias[$i];
+echo "O nadador ",$nome, "compete na categoria infantil",$categorias[$i];
 }}
 
 else if($idade >= 13 && $idade <=18)
 
 {for($i = 0; $i <= count ($categorias); $i++)
 {if($categorias[$i] == 'adolescente')
-echo "O nadador",$nome, "compete na categoria adolescente";
+echo "O nadador ",$nome, "compete na categoria adolescente";
 }}
 
 else if($idade >= 19 && $idade <=100)
