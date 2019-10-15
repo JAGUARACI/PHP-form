8af3becb0e929037b605a6f6f512fcbf6f0ef1b1
JAGUARACI <jaguaracisudu@gmaile.com>
2019-10-15 17:04:56 -0300

    corr1

--- script.php
+++ script.php
@@ -20,25 +20,25 @@ $idade = $_POST['idade'];
 
 if(empty($nome))
 {$_SESSION['mensagem-de-erro'] = 'O nome não pode ser vazio por favor preencha-o novamente';
-    header( string "location:index.php");
+    header( string 'location:index.php');
     return;
 }
 
 else if(strlen($nome) < 3)
 {$_SESSION['mensagem-de-erro'] = 'O nome não pode ter menos de três caracteres';
-    header( string "location: index.php");
+    header( string 'location: index.php');
     return;
 }
 
 else if(strlen($nome) > 40)
 {$_SESSION['mensagem-de-erro'] = 'O nome não pode ser ter mais de quarenta caracteres';
-    header( string "location: index.php");
+    header( string 'location: index.php');
     return;
 }
 
 else if(!is_numeric($idade))
 {$_SESSION['mensagem-de-erro'] = 'Informe um número para idade';
-    header( string: "location: index.php");
+    header( string: 'location: index.php');
     return;
     }
 
