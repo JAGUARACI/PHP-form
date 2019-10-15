0766971d2c947b8d303b1d6ecc7066c2e0e6224a
JAGUARACI <jaguaracisudu@gmaile.com>
2019-10-15 13:53:31 -0300

    numeric

--- script.php
+++ script.php
@@ -31,6 +31,11 @@ if(strlen($nome) > 40)
 return;
 }
 
+if(!is_numeric($idade))
+{echo 'informe um número para idade';
+}
+return;
+
 if($idade >= 6 && $idade <=12)
 
 {for($i = 0; $i <= count ($categorias); $i++)
